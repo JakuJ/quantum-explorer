58ddef903d83257fe8f9ac392a381c39a65c8436
Jakub Janaszkiewicz <janaszkiewicz.jakub@gmail.com>
2020-07-17 11:50:49 +0200

    Get rid of the sidebar, document extension methods

--- Quantum-Server/Extensions/Conversions.cs
+++ Quantum-Server/Extensions/Conversions.cs
@@ -3,11 +3,21 @@ using Microsoft.Quantum.Simulation.Core;
 
 namespace Quantum_Server.Extensions
 {
+    /// <summary>
+    /// Extension methods regarding conversions.
+    /// </summary>
     public static class Conversions
     {
-        public static QArray<T> ToQArray<T>(this IEnumerable<T> arr)
+        /// <summary>
+        /// Cast an arbitrary <see cref="IEnumerable{T}"/> to a <see cref="QArray{T}"/> so it can be used in
+        /// the quantum simulator context.
+        /// </summary>
+        /// <param name="collection">An arbitrary enumerable collection of items.</param>
+        /// <typeparam name="T">Type parameter.</typeparam>
+        /// <returns>An instance of <see cref="QArray{T}"/>.</returns>
+        public static QArray<T> ToQArray<T>(this IEnumerable<T> collection)
         {
-            return new QArray<T>(arr);
+            return new QArray<T>(collection);
         }
     }
 }
