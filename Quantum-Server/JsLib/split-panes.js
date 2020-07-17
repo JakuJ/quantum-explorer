1620606fc3c22b433c7e5d8910c7d77c6fe75b87
Jakub Janaszkiewicz <janaszkiewicz.jakub@gmail.com>
2020-07-17 17:11:08 +0200

    Add background images to gutters

--- Quantum-Server/JsLib/split-panes.js
+++ Quantum-Server/JsLib/split-panes.js
@@ -4,13 +4,11 @@ export function initializeSplitPanes() {
   Split(['#left-pane', '#right-pane'], {
     gutterSize: 8,
     sizes: [50, 50],
-    cursor: 'col-resize'
   });
 
   Split(['#upper-pane', '#lower-pane'], {
     direction: 'vertical',
     sizes: [50, 50],
     gutterSize: 8,
-    cursor: 'row-resize'
   });
 }
