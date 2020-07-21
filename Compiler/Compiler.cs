c4f6cd39f15233d5bee59fcd4e60c89035e98894
Jakub Janaszkiewicz <janaszkiewicz.jakub@gmail.com>
2020-07-21 23:19:34 +0200

    Loading core Q# DLLs

--- Compiler/Compiler.cs
+++ Compiler/Compiler.cs
@@ -1,12 +1,16 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.Quantum.QsCompiler.CompilationBuilder;
 using Microsoft.Quantum.QsCompiler.DataTypes;
 using Microsoft.Quantum.QsCompiler.SyntaxTree;
 using Microsoft.Quantum.QsCompiler.Transformations.QsCodeOutput;
 using Compilation = Microsoft.Quantum.QsCompiler.CompilationBuilder.CompilationUnitManager.Compilation;
+using Microsoft.Quantum.Intrinsic;
 
 namespace CompilerExtensions
 {
@@ -14,13 +18,35 @@ namespace CompilerExtensions
     {
         private Compilation compilation;
 
+        static string GetDllPath(string dll)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            string assemblyDir = Path.GetDirectoryName(assembly.Location);
+            if (assemblyDir == null)
+            {
+                throw new ApplicationException("Couldn't get the directory of the executing assembly");
+            }
+
+            return Path.Combine(assemblyDir, dll);
+        }
+
         public async Task Compile(string filepath)
         {
-            var sourceFiles = ProjectManager.LoadSourceFiles(new[] {filepath});
-            var files = CompilationUnitManager.InitializeFileManagers(sourceFiles);
+            ImmutableDictionary<Uri, string> sourceFiles = ProjectManager.LoadSourceFiles(new[] {filepath});
+            ImmutableHashSet<FileContentManager> files = CompilationUnitManager.InitializeFileManagers(sourceFiles);
 
             using var manager = new CompilationUnitManager();
-            await manager.UpdateReferencesAsync(new References(ImmutableDictionary<NonNullable<string>, References.Headers>.Empty));
+
+            string[] paths =
+            {
+                GetDllPath("Microsoft.Quantum.Standard.dll"),
+                GetDllPath("Microsoft.Quantum.QSharp.Core.dll"),
+            };
+
+            ImmutableDictionary<NonNullable<string>, References.Headers> dict = ProjectManager.LoadReferencedAssemblies(paths);
+            await manager.UpdateReferencesAsync(new References(dict));
+
             await manager.AddOrUpdateSourceFilesAsync(files);
             compilation = manager.Build();
         }
---
+++ QSharpTestSources/Library.qs
@@ -0,0 +1,27 @@
+﻿namespace QSharpTestSources {
+
+    open Microsoft.Quantum.Canon;
+    open Microsoft.Quantum.Intrinsic;
+    open Microsoft.Quantum.Measurement;
+    open Microsoft.Quantum.Convert;
+    open Microsoft.Quantum.Math;
+
+    operation RandomBit () : Result {
+        using (q = Qubit()) {
+            H(q);
+            return MResetZ(q);
+        }
+    }
+
+    operation RandomInt(max: Int) : Int {
+        mutable result = 0;
+        repeat {
+            mutable bits = new Result[0];
+            for (i in 1 .. BitSizeI(max)) {
+                set bits += [RandomBit()];
+            }
+            set result = ResultArrayAsInt(bits);
+        } until (result <= max);
+        return result;
+    }
+}
