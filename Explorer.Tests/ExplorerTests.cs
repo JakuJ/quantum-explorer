6066df8d5d6f162fc915f847f86c291b523daeb6
skdw <kamil.gorzynski8@gmail.com>
2020-07-23 16:06:50 +0200

    Add NUnit framework tests project [#1]

---
+++ Explorer.Tests/ExplorerTests.cs
@@ -0,0 +1,21 @@
+using NUnit.Framework;
+using Quantum_Server;
+using Quantum_Server.Extensions;
+
+namespace Explorer.Tests
+{
+    public class Tests
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test()
+        {
+            Assert.Pass();
+        }
+    }
+}
