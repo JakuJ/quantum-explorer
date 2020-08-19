cb4030067e478864ee0165f7bee4c02bdcf2d653
Jakub Janaszkiewicz <janaszkiewicz.jakub@gmail.com>
2020-08-19 14:46:32 +0200

    Tests for the Index page

--- Explorer.Tests/IndexPageTest.cs
+++ Explorer.Tests/IndexPageTest.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Text.RegularExpressions;
 using Bunit;
 using Bunit.TestDoubles.JSInterop;
 using Compiler;
@@ -13,11 +15,13 @@ namespace Explorer.Tests
     [Parallelizable]
     public class IndexPageTest
     {
+        private const string TestCode = "namespace Test { function Test () : Unit { } }";
+
         private static TestContext InitializeContext()
         {
             var ctx = new TestContext();
             ctx.Services.AddMockJSRuntime();
-            ctx.Services.AddSingleton<ICompiler>(new MockICompiler());
+            ctx.Services.AddSingleton<ICompiler>(new QsCompiler());
             return ctx;
         }
 
@@ -28,5 +32,31 @@ namespace Explorer.Tests
             var index = ctx.RenderComponent<Index>();
             Assert.AreEqual(3, index.FindComponents<Resizable>().Count, "There should be three Resizable panels at the beginning");
         }
+
+        [Test]
+        public void PrettyPrintsCode()
+        {
+            using var ctx = InitializeContext();
+            var index = ctx.RenderComponent<Index>();
+            index.Instance.Editor.Component.Code = TestCode;
+
+            Assert.DoesNotThrow(
+                () =>
+                {
+                    var button = index.Find("button#compile");
+                    button.Click();
+                }, "Clicking on the compile button should not throw");
+
+            index.WaitForState(() => !index.Instance.OutputEditor.Component.Code.StartsWith("The"), TimeSpan.FromSeconds(5));
+
+            Assert.AreEqual(0, index.Instance.Diagnostics.Component.Diagnostics.Count, "There should be no compiler diagnostics");
+
+            Assert.AreEqual(
+                NormalizeWhitespace(TestCode),
+                NormalizeWhitespace(index.Instance.OutputEditor.Component.Code),
+                "Pretty-printed code should be the same as the original.");
+        }
+
+        private static string NormalizeWhitespace(string input) => new Regex(@"\s+").Replace(input, " ");
     }
 }
