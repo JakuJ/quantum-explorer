e5339fbb255f0ad84374ba848ae13581390f3dec
Kamil Gorzynski <kamil.gorzynski8@gmail.com>
2020-11-24 02:40:52 +0100

    Mock composer test environment

--- Explorer.Tests/ComposerTest.cs
+++ Explorer.Tests/ComposerTest.cs
@@ -1,9 +1,14 @@
 using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 using Bunit;
 using Bunit.TestDoubles.JSInterop;
 using Compiler;
 using Explorer.Components.Composer;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Moq;
 using NUnit.Framework;
 
 namespace Explorer.Tests
@@ -12,12 +17,33 @@ namespace Explorer.Tests
     [Parallelizable]
     public class ComposerTest
     {
+        /// <summary>
+        /// Renders the composer.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the result of the asynchronous operation.</returns>
         [Test]
         public async Task RendersComposer()
         {
             // Arrange
+            var repoDir = Directory
+                .GetParent(Directory.GetCurrentDirectory())
+                .Parent.Parent.Parent.FullName;
+            var wwwRoot = Path.Join(repoDir, "Explorer", "wwwroot");
+
+            var mockEnvironment = new Mock<IWebHostEnvironment>();
+            mockEnvironment
+                .Setup(m => m.ApplicationName)
+                .Returns("ComposerTest");
+            mockEnvironment
+                .Setup(m => m.EnvironmentName)
+                .Returns("UnitTestEnvironment");
+            mockEnvironment
+                .Setup(m => m.WebRootPath)
+                .Returns(wwwRoot);
+
             using var ctx = new Bunit.TestContext();
             ctx.Services.AddMockJSRuntime();
+            ctx.Services.TryAddSingleton<IWebHostEnvironment>(_ => mockEnvironment.Object);
             var cut = ctx.RenderComponent<Composer>();
 
             var grid = new GateGrid();
@@ -29,12 +55,12 @@ namespace Explorer.Tests
             await cut.InvokeAsync(async () => await cut.Instance.UpdateGrids(ast));
 
             // Assert
+            // Check if the gate name is displayed
             cut.Find(".gate-name").TextContent.MarkupMatches("H");
 
-            // it doesn't work, the paths are different for unit testing so the photo cannot be loaded
-            // we have to get the proper wwwroot location by injecting IWebHostEnvironment
-            // cut.Find(".img").MarkupMatches("MResetZ");
-            return;
+            // Check if the icon is displayed
+            var src = cut.Find("img").Attributes.GetNamedItem("src");
+            Assert.AreEqual(src.Value, "images/icons/MResetZ.svg");
         }
     }
 }
