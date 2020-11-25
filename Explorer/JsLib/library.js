2b71370a4708f5e21ccde5ebb48c7b6b037fa39c
Jakub Janaszkiewicz <janaszkiewicz.jakub@gmail.com>
2020-11-25 19:43:15 +0100

    Dark mode

--- Explorer/JsLib/library.js
+++ Explorer/JsLib/library.js
@@ -1,5 +1,7 @@
-import { initializeSplitPanes } from './split-panes';
-export { Editor } from './editor';
+import {initializeSplitPanes} from './split-panes';
+
+export {toggleTheme} from './theme';
+export {Editor} from './editor';
 
 export function InitializeSplitPanes(panelJSON) {
   initializeSplitPanes(JSON.parse(panelJSON));
