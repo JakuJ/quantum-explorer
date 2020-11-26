7831bdd5a46c015ef7a699b75dba88f97233e754
Adam Kowalczyk <adam.filip.kowalczyk@gmail.com>
2020-11-26 21:11:08 +0100

    Set up DB connection from correct project

--- DatabaseHandler/CodeDbContext.cs
+++ DatabaseHandler/CodeDbContext.cs
@@ -1,6 +1,9 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace DatabaseHandler
@@ -10,5 +13,20 @@ namespace DatabaseHandler
         public CodeDbContext(DbContextOptions<CodeDbContext> options) : base(options) { }
 
         public virtual DbSet<CodeInformation> CodeInformations { get; set; }
+    }
+
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<CodeDbContext>
+    {
+        public CodeDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("./appsettings.json")
+                .Build();
+            var builder = new DbContextOptionsBuilder<CodeDbContext>();
+            var connectionString = configuration.GetConnectionString("DatabaseConnection");
+            builder.UseSqlServer(connectionString);
+            return new CodeDbContext(builder.Options);
+        }
     }
 }
