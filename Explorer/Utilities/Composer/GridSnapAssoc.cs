3494b408b2b09444e5d8d759a79af4784230b347
Kamil Gorzynski <kamil.gorzynski8@gmail.com>
2020-11-26 03:03:36 +0100

    GridSnapAssoc fix

--- Explorer/Utilities/Composer/GridSnapAssoc.cs
+++ Explorer/Utilities/Composer/GridSnapAssoc.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.JSInterop;
 
-namespace Explorer.Utilities.ComponentTree
+namespace Explorer.Utilities.Composer
 {
     /// <summary>
     /// Handles the associations between the grid gates and snaps.
