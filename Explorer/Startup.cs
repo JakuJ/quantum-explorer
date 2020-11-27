17c13af3e4c647248f2d6a223b76eac38887061f
Jakub Janaszkiewicz <janaszkiewicz.jakub@gmail.com>
2020-11-27 14:55:22 +0100

    Missing Explorer docs

--- Explorer/Startup.cs
+++ Explorer/Startup.cs
@@ -17,7 +17,6 @@ namespace Explorer
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsProduction())
