f457bbf5786ac6fcfe31264eeaa806fc5cd93949
Adam Kowalczyk <adam.filip.kowalczyk@gmail.com>
2020-11-27 13:02:39 +0100

    Add test project for DatabaseHandler

---
+++ DatabaseHandler.Tests/CodeDatabaseHandlerTests.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace DatabaseHandler.Tests
+{
+    public class CodeDatabaseHandlerTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
