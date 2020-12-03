00fafad9a5cf4a71a7dd85d8c073924bbf8534b7
Adam Kowalczyk <adam.filip.kowalczyk@gmail.com>
2020-12-03 23:43:29 +0100

    Add share component

--- Explorer/JsLib/library.js
+++ Explorer/JsLib/library.js
@@ -1,16 +1,24 @@
 // This is the main module, exposed globally as "Library".
 // It exports all objects and methods used by Blazor.
 
-export {initializeSplitPanes} from './split-panes';
-export {toggleTheme} from './theme';
-export {Editor} from './editor';
+export { initializeSplitPanes } from './split-panes';
+export { toggleTheme } from './theme';
+export { Editor } from './editor';
+export { showSharePopOver, initPopOverDestroyer} from './share';
 
 // Helper functions for disabling and enabling DOM elements
 
 export function disable(ref) {
-  ref.disabled = true;
+    ref.disabled = true;
 }
 
 export function enable(ref) {
-  ref.disabled = false;
+    ref.disabled = false;
+}
+
+export function copyToClipboard(text) {
+    navigator.clipboard.writeText(text)
+        .catch(function (error) {
+            alert(error);
+        });
 }
