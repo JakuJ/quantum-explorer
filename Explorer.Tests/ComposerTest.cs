7f638f105318801323407d9199aad556a520029e
Kamil Gorzynski <kamil.gorzynski8@gmail.com>
2020-12-10 15:24:28 +0100

    Add more composer tests

--- Explorer.Tests/ComposerTest.cs
+++ Explorer.Tests/ComposerTest.cs
@@ -37,11 +37,21 @@ namespace Explorer.Tests
             grid.AddGate(2, new QuantumGate("X"));
             grid.AddGate(3, CustomGateFactory.MakeCustomGate("__control__"));
             grid.AddGate(4, new QuantumGate("ResetAll"));
-            Dictionary<string, List<GateGrid>> ast = new() { { "ExampleNamespace.Tab1", new List<GateGrid>() { grid } } };
+
+            Dictionary<string, List<GateGrid>> astTab = new()
+            {
+                { "ExampleNamespace.Tab1", new List<GateGrid>() { grid } },
+            };
+            Dictionary<string, List<GateGrid>> astTabs = new()
+            {
+                { "ExampleNamespace.Tab1", new List<GateGrid>() { grid } },
+                { "ExampleNamespace.Tab2", new List<GateGrid>() { grid } },
+            };
 
             // Act
             IRenderedComponent<Composer> cut = ctx.RenderComponent<Composer>();
-            await cut.InvokeAsync(async () => await cut.Instance.UpdateGridsAsync(ast));
+            await cut.InvokeAsync(async () => await cut.Instance.UpdateGridsAsync(astTab));
+            await cut.InvokeAsync(async () => await cut.Instance.UpdateGridsAsync(astTabs));
 
             // Assert
             // Check if the Grid component is rendered
@@ -53,6 +63,40 @@ namespace Explorer.Tests
             // Check if the icon is displayed
             var gateSrc = gridComponent.Find("#Measurement_Gate");
             Assert.AreEqual(gateSrc.InnerHtml.Length, 682);
+        }
+
+        [Test]
+        public void ThrowsInvalidOperation()
+        {
+            // Arrange
+            using TestContext ctx = new();
+            ctx.Services.AddMockJSRuntime();
+            ctx.Services.TryAddScoped(_ => new Mock<ILogger<Composer>>().Object);
+            ctx.Services.TryAddScoped(_ => new Mock<ILogger<Grid>>().Object);
+            ctx.Services.TryAddScoped(_ => new Mock<ILogger<Gate>>().Object);
+            ctx.Services.TryAddScoped(_ => new Mock<ILogger<Cell>>().Object);
+            ctx.Services.TryAddScoped(_ => new Mock<ILogger<SnapPoint>>().Object);
+            ctx.Services.TryAddSingleton(_ => Helpers.GetMockEnvironment().Object);
+
+            IRenderedComponent<Composer> cut = ctx.RenderComponent<Composer>();
+            Dictionary<string, List<GateGrid>> astEmpty = new();
+
+            // Act & Assert - check if setting the empty AST throws an exception
+            Assert.ThrowsAsync<System.InvalidOperationException>(
+                async () => await cut.InvokeAsync(async () => await cut.Instance.UpdateGridsAsync(astEmpty)));
+        }
+
+        [Test]
+        public void RendersCircle()
+        {
+            // Arrange
+            using TestContext ctx = new();
+
+            // Act
+            IRenderedComponent<Circle> cut = ctx.RenderComponent<Circle>(("Stroke", "red"));
+
+            // Assert
+            Assert.AreEqual(cut.Markup.Length, 129);
         }
     }
 }
