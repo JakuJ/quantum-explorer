b78f7a8f6353ef31164458d08a4ee853acc634c7
Adam Kowalczyk <adam.filip.kowalczyk@gmail.com>
2020-12-10 16:58:50 +0100

    Add compilation test for code examples

---
+++ Explorer.Tests/ExamplesCompilationTest.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Bunit;
+using Bunit.TestDoubles;
+using Compiler;
+using Explorer.Components;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using TestContext = Bunit.TestContext;
+
+namespace Explorer.Tests
+{
+    [Parallelizable]
+    [TestFixture]
+    public class ExamplesCompilationTest
+    {
+        [TestCase("./CodeExamples/")]
+        public async Task ExamplesCompiles(string directoryPath)
+        {
+            // Arrange
+            var codes = GetExampleCodes(directoryPath);
+            QsCompiler compiler = new(Mock.Of<ILogger>());
+            bool runs = false;
+
+            compiler.OnOutput += (_, _) => { runs = true; };
+
+            foreach (var codeInfo in codes)
+            {
+                // Act
+                runs = false;
+                await compiler.Compile(codeInfo.Code);
+
+                // Assert
+                Assert.IsTrue(runs, $"Code from {codeInfo.Name} should compile.");
+            }
+
+        }
+
+        private List<(string Name, string Code)> GetExampleCodes(string folderPath)
+        {
+            List<(string Name, string Code)> codes = new();
+            if (!Directory.Exists(folderPath))
+            {
+                throw new DirectoryNotFoundException();
+            }
+
+            foreach (string file in Directory.EnumerateFiles(folderPath, "*.qs"))
+            {
+                string name = Path.GetFileNameWithoutExtension(file);
+                string code = File.ReadAllText(file);
+                codes.Add((name, code));
+            }
+
+            return codes;
+        }
+    }
+}
