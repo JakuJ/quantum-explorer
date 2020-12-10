3c31117a2cce5a40649c19f9692f95685cac56f8
Kamil Gorzynski <kamil.gorzynski8@gmail.com>
2020-12-10 16:03:31 +0100

    ComposerTest update

--- Explorer.Tests/ComposerTest.cs
+++ Explorer.Tests/ComposerTest.cs
@@ -38,14 +38,17 @@ namespace Explorer.Tests
             grid.AddGate(3, CustomGateFactory.MakeCustomGate("__control__"));
             grid.AddGate(4, new QuantumGate("ResetAll"));
 
+            GateGrid grid2 = new();
+            grid2.AddGate(0, new QuantumGate("H"));
+
             Dictionary<string, List<GateGrid>> astTab = new()
             {
-                { "ExampleNamespace.Tab1", new List<GateGrid>() { grid } },
+                { "TabWithoutNamespace", new List<GateGrid>() { grid } },
             };
             Dictionary<string, List<GateGrid>> astTabs = new()
             {
                 { "ExampleNamespace.Tab1", new List<GateGrid>() { grid } },
-                { "ExampleNamespace.Tab2", new List<GateGrid>() { grid } },
+                { "ExampleNamespace.Tab2", new List<GateGrid>() { grid2 } },
             };
 
             // Act
