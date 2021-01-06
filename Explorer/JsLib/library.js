a791e3e1d35f750aa4e7caa6a6c682b8ae442880
Adam Kowalczyk <adam.filip.kowalczyk@gmail.com>
2021-01-06 22:25:27 +0100

    Fix formatting

--- Explorer/JsLib/library.js
+++ Explorer/JsLib/library.js
@@ -28,16 +28,16 @@ export function changeUrl(url) {
 }
 
 export function saveTextFile(filename, data) {
-    var blob = new Blob([data], { type: 'text/csv' });
-    if (window.navigator.msSaveOrOpenBlob) {
-        window.navigator.msSaveBlob(blob, filename);
-    }
-    else {
-        var elem = window.document.createElement('a');
-        elem.href = window.URL.createObjectURL(blob);
-        elem.download = filename;
-        document.body.appendChild(elem);
-        elem.click();
-        document.body.removeChild(elem);
-    }
+  var blob = new Blob([data], { type: 'text/csv' });
+  if (window.navigator.msSaveOrOpenBlob) {
+    window.navigator.msSaveBlob(blob, filename);
+  }
+  else {
+    var elem = window.document.createElement('a');
+    elem.href = window.URL.createObjectURL(blob);
+    elem.download = filename;
+    document.body.appendChild(elem);
+    elem.click();
+    document.body.removeChild(elem);
+  }
 }
