8ba25330228b8c0d0f240f45b6c42d28ca69b5b8
Jakub Janaszkiewicz <janaszkiewicz.jakub@gmail.com>
2021-01-12 22:08:46 +0100

    Refactor AddGateMenuTest

--- Explorer.Tests/AddGateMenuTest.cs
+++ Explorer.Tests/AddGateMenuTest.cs
@@ -1,3 +1,4 @@
+using AngleSharp.Dom;
 using Bunit;
 using Explorer.Components.Composer;
 using NUnit.Framework;
@@ -9,94 +10,27 @@ namespace Explorer.Tests
     [Parallelizable]
     public class AddGateMenuTest
     {
-        [Test]
-        public void AddsPauliXGate()
+        [TestCase("X", 0)]
+        [TestCase("Y", 1)]
+        [TestCase("Z", 2)]
+        [TestCase("H", 3)]
+        [TestCase("__control__", 4)]
+        [TestCase("MResetZ", 5)]
+        public void AddsGate(string name, int index)
         {
             // Arrange
             using TestContext ctx = new();
-            string? temp = "";
+            var temp = "";
 
             // Act
             IRenderedComponent<AddGateMenu> cut = ctx.RenderComponent<AddGateMenu>(parameters => parameters
-                .Add(p => p.ExitMenu, gateName => { temp = gateName; }));
+                                                                                      .Add(p => p.ExitMenu, gateName => { temp = gateName; }));
 
-            var item = cut.FindAll(".dropdown-item");
-            item[0].Click(); // click the 1. item - Pauli-X
-
-            // Assert
-            Assert.AreEqual("X", temp);
-        }
-
-        [Test]
-        public void AddsPauliYGate()
-        {
-            // Arrange
-            using TestContext ctx = new();
-            string? temp = "";
-
-            // Act
-            IRenderedComponent<AddGateMenu> cut = ctx.RenderComponent<AddGateMenu>(parameters => parameters
-                .Add(p => p.ExitMenu, gateName => { temp = gateName; }));
-
-            var item = cut.FindAll(".dropdown-item");
-            item[1].Click(); // click the 2. item - Pauli-Y
+            IRefreshableElementCollection<IElement> item = cut.FindAll(".dropdown-item");
+            item[index].Click();
 
             // Assert
-            Assert.AreEqual("Y", temp);
-        }
-
-        [Test]
-        public void AddsPauliZGate()
-        {
-            // Arrange
-            using TestContext ctx = new();
-            string? temp = "";
-
-            // Act
-            IRenderedComponent<AddGateMenu> cut = ctx.RenderComponent<AddGateMenu>(parameters => parameters
-                .Add(p => p.ExitMenu, gateName => { temp = gateName; }));
-
-            var item = cut.FindAll(".dropdown-item");
-            item[2].Click(); // click the 3. item - Pauli-Z
-
-            // Assert
-            Assert.AreEqual("Z", temp);
-        }
-
-        [Test]
-        public void AddsHadamardGate()
-        {
-            // Arrange
-            using TestContext ctx = new();
-            string? temp = "";
-
-            // Act
-            IRenderedComponent<AddGateMenu> cut = ctx.RenderComponent<AddGateMenu>(parameters => parameters
-                .Add(p => p.ExitMenu, gateName => { temp = gateName; }));
-
-            var item = cut.FindAll(".dropdown-item");
-            item[3].Click(); // click the 4. item - Hadamard
-
-            // Assert
-            Assert.AreEqual("H", temp);
-        }
-
-        [Test]
-        public void AddsMResetXGate()
-        {
-            // Arrange
-            using TestContext ctx = new();
-            string? temp = "";
-
-            // Act
-            IRenderedComponent<AddGateMenu> cut = ctx.RenderComponent<AddGateMenu>(parameters => parameters
-                .Add(p => p.ExitMenu, gateName => { temp = gateName; }));
-
-            var item = cut.FindAll(".dropdown-item");
-            item[4].Click(); // click the 5. item - MResetX
-
-            // Assert
-            Assert.AreEqual("MResetX", temp);
+            Assert.AreEqual(name, temp, $"Gate with name {name} is expected after clicking menu item {index}");
         }
     }
 }
